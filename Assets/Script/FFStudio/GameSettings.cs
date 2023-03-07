aaf97ac5a37736a85713fe2c95a5a799289d0658
ofgonen_office <ofgonen@gmail.com>
2023-03-08 01:22:00 +0300

    GameSettings: Selection Layer

--- Assets/Script/FFStudio/GameSettings.cs
+++ Assets/Script/FFStudio/GameSettings.cs
@@ -10,7 +10,9 @@ namespace FFStudio
     {
 #region Fields (Settings)
     // Info: You can use Title() attribute ONCE for every game-specific group of settings.
-    
+    [ Title( "Selection" ) ]
+        [ LabelText( "Selection Layer" ), Layer() ] public int selection_layer;
+
     [ Title( "Camera" ) ]
         [ LabelText( "Follow Speed (Z)" ), SuffixLabel( "units/seconds" ), Min( 0 ) ] public float camera_follow_speed_depth = 2.8f;
     
