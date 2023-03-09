b85bdfe1e27eb335811a1d9c358eb221583175db
ofgonen-home <ofgonen@gmail.com>
2023-03-09 04:29:12 +0300

    Extension: Currency Key

--- Assets/Script/FFStudio/Extension/Extensions.cs
+++ Assets/Script/FFStudio/Extension/Extensions.cs
@@ -11,8 +11,9 @@ namespace FFStudio
 {
 	public static class Extensions
 	{
-		public static readonly string SAVE_PATH   = Application.persistentDataPath + "/Saves/";
-		public static readonly string Key_Economy = "Economy";
+		public static readonly string SAVE_PATH    = Application.persistentDataPath + "/Saves/";
+		public static readonly string Key_Economy  = "Economy";
+		public static readonly string Key_Currency = "Currency";
 
 		static List< Transform > baseModelBones   = new List< Transform >( 96 );
 		static List< Transform > targetModelBones = new List< Transform >( 96 );
