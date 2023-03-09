64ff1b42dda93b48124373f40ad5e02bea85d0bf
ofgonen-home <ofgonen@gmail.com>
2023-03-09 04:39:26 +0300

    Create and Configure PoolPiggyBank

--- Assets/Script/FFStudio/Manager/AssetManager.cs
+++ Assets/Script/FFStudio/Manager/AssetManager.cs
@@ -22,6 +22,7 @@ namespace FFStudio
 
 	[ Title( "Pool" ) ]
 		[ SerializeField ] Pool_UIPopUpText pool_UIPopUpText;
+		[ SerializeField ] PoolPiggyBank pool_piggyBank;
 #endregion
 
 #region UnityAPI
@@ -35,6 +36,7 @@ namespace FFStudio
 			Vibration.Init();
 
 			pool_UIPopUpText.InitPool( transform, false );
+			pool_piggyBank.InitPool( transform, false );
 			onAwakeEvent.Invoke();
 		}
 
