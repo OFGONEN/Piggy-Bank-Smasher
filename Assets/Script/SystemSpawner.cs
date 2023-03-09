fa33c0f240de63cce535c8ffae47420549ba616a
ofgonen-home <ofgonen@gmail.com>
2023-03-09 11:02:09 +0300

    Create & Configure System Spawner

---
+++ Assets/Script/SystemSpawner.cs
@@ -0,0 +1,36 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+
+[ CreateAssetMenu( fileName = "system_spawner", menuName = "FF/Game/Spawner System" ) ]
+public class SystemSpawner : ScriptableObject
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void OnLevelStarted()
+    {
+
+    }
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
