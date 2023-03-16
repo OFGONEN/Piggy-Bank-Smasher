c2002298bd2bef010941dc9d9df8de48c3d6c1c0
ofgonen-home <ofgonen@gmail.com>
2023-03-16 18:22:13 +0300

    UIUnlockSequence: Implement Target image sequene and add cooldown to end the sequence

--- Assets/Script/UIUnlockSequence.cs
+++ Assets/Script/UIUnlockSequence.cs
@@ -17,6 +17,11 @@ public class UIUnlockSequence : MonoBehaviour
     [ SerializeField ] Ease tween_text_rotation_ease;
     [ SerializeField ] float tween_text_rotation_interval;
     [ SerializeField ] float tween_glow_rotation_speed;
+    [ SerializeField ] float tween_image_scale_start;
+    [ SerializeField ] float tween_image_scale_duration;
+    [ SerializeField ] Ease tween_image_scale_ease;
+    [ SerializeField ] PunchScaleTween tween_image_punchScale;
+    [ SerializeField ] float sequence_cooldown;
 
   [ Title( "Components" ) ]
     [ SerializeField ] GameObject gfx_parent;
@@ -25,9 +30,13 @@ public class UIUnlockSequence : MonoBehaviour
     [ SerializeField ] Image image_piggyBank_target;
     [ SerializeField ] Button _button;
 
+  [ Title( "Shared" ) ]
+	[ SerializeField ] SystemEconomy system_economy;
+
     RecycledSequence recycledSequence_text = new RecycledSequence();
     RecycledTween    recycledTween_glow    = new RecycledTween();
     RecycledTween    recycledTween_target  = new RecycledTween();
+	Cooldown cooldown = new Cooldown();
 #endregion
 
 #region Properties
@@ -40,14 +49,22 @@ public class UIUnlockSequence : MonoBehaviour
     public void OnEconomyUnlock()
     {
 		Time.timeScale = 0;
-
-		_button.interactable = true;
+		_button.interactable = false;
 
 		gfx_parent.SetActive( true );
 
-		image_piggyBank_target.rectTransform.localScale = Vector3.one;
+		image_piggyBank_target.rectTransform.localScale = Vector3.one * tween_image_scale_start;
 		transform_text.eulerAngles                      = Vector3.forward * -tween_text_rotation_value;
 		transform_glow.rotation                         = Quaternion.identity;
+		image_piggyBank_target.sprite                   = system_economy.UnlockSprite;
+
+		recycledTween_target.Recycle( image_piggyBank_target.rectTransform.DOScale(
+			1,
+			tween_image_scale_duration )
+			.SetEase( tween_image_scale_ease )
+			.SetUpdate( true ),
+			OnTargetImageScaleDone
+		);
 
 		var sequenceText = recycledSequence_text.Recycle();
 
@@ -83,12 +100,22 @@ public class UIUnlockSequence : MonoBehaviour
 		recycledSequence_text.Kill();
 		recycledTween_glow.Kill();
 		recycledTween_target.Kill();
+		cooldown.Kill();
 
 		Time.timeScale = 1;
 	}
 #endregion
 
 #region Implementation
+	void OnTargetImageScaleDone()
+	{
+		_button.interactable = true;
+
+		recycledTween_target.Recycle( tween_image_punchScale.CreateTween( image_piggyBank_target.rectTransform )
+		.SetUpdate( true ) );
+
+		cooldown.StartUnscaled( sequence_cooldown, _button.onClick.Invoke );
+	}
 #endregion
 
 #region Editor Only
