bdd9772c6187c15b9141f6e2ce65358a36b10f94
ofgonen-home <ofgonen@gmail.com>
2023-03-17 15:47:00 +0300

    Implement UICurrencyText

---
+++ Assets/Script/UICurrenyText.cs
@@ -0,0 +1,50 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using FFStudio;
+
+public class UICurrenyText : MonoBehaviour
+{
+#region Fields
+    [ SerializeField ] Currency notif_currency;
+    [ SerializeField ] TextMeshProUGUI _textRenderer;
+    [ SerializeField ] float maxValue;
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+    private void OnEnable()
+    {
+		notif_currency.Subscribe( OnCurrencyChanged );
+		OnCurrencyChanged();
+	}
+
+    private void OnDisable()
+    {
+		notif_currency.Unsubscribe( OnCurrencyChanged );
+	}
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+    void OnCurrencyChanged()
+    {
+        if( notif_currency.sharedValue >= maxValue )
+			_textRenderer.text = MathExtensions.FormatBigNumberAANotation( notif_currency.sharedValue );
+        else
+			_textRenderer.text = notif_currency.sharedValue.ToString();
+	}
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
